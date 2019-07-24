4d0b71f12b3b3976ab614f5da1cb38a6c2848d01
tjr226 <tjr226@gmail.com>
2019-07-23 23:09:30 -0400

    mvp complete

--- api/setup-middleware.js
+++ api/setup-middleware.js
@@ -2,11 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session); // remember to add (session)
 
 module.exports = server => {
     const sessionConfig = {
         name: 'monkey', // sid session ID
-        secret: 'keep it secret, keep it safe',
+        secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe',
         cookie: {
             maxAge: 1000 * 60 * 5, // in milliseconds
             secure: false, // limits to sending over https, needs to be true in production
@@ -14,6 +15,14 @@ module.exports = server => {
         },
         resave: false,
         saveUninitialized: false, // gpdr compliance
+        store: new KnexSessionStore({ // remember to add NEW here
+            knex: require('../database/dbConfig.js'),
+            tablename: 'sessions',
+            createtable: true,
+            sidfieldname: 'sid',
+            clearInterval: 1000 * 60 * 60, // deletes expired session data every hour
+            
+        })
     }
     server.use(helmet());
     server.use(express.json());
